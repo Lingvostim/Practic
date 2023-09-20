576a1073c32e17a93706c00047d472f734380180
kirill <golub.kirill12@gmail.com>
2023-09-20 12:16:50 +0300

    Initial Commit

---
+++ Practic1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practic1
+//
+//  Created by Kirill Ter on 20.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
